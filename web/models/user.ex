be8560785b09255ee594526df8df76f0cafeb404
Chris Crawford <chris.a.crawford@gmail.com>
2017-07-10 11:22:48 -0500

    Show user name rather than email

--- web/models/user.ex
+++ web/models/user.ex
@@ -3,10 +3,15 @@ defmodule Bestpest.User do
   alias Bestpest.Repo
 
   schema "users" do
+    field :name, :string
     field :email, :string
     field :encrypted_password, :string
 
     timestamps()
+  end
+
+  def fetch_name_by_email(user) do
+    Repo.get_by!(Bestpest.User, email: user.email).name
   end
 
 end
