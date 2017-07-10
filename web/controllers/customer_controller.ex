0b76b1c3c62eddb673891b07184d827f82960794
Chris Crawford <chris.a.crawford@gmail.com>
2017-07-10 11:36:48 -0500

    Implement customer list template

--- web/controllers/customer_controller.ex
+++ web/controllers/customer_controller.ex
@@ -1,12 +1,15 @@
 defmodule Bestpest.CustomerController do
   use Bestpest.Web, :controller
 
+  alias Bestpest.Customer
+
   plug Addict.Plugs.Authenticated when action in [
     :index
   ]
   plug :action
 
   def index(conn, _params) do
-    render conn, "index.html"
+    customers = Repo.all(Customer)
+    render conn, "index.html", customers: customers
   end
 end
