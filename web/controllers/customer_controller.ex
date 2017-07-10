e67e9bd55d0a16fbd67fc4ce9974fd99b0b32743
Chris Crawford <chris.a.crawford@gmail.com>
2017-07-10 16:49:20 -0500

    Dispay individual customer information

--- web/controllers/customer_controller.ex
+++ web/controllers/customer_controller.ex
@@ -12,4 +12,34 @@ defmodule Bestpest.CustomerController do
     customers = Repo.all(Customer)
     render conn, "index.html", customers: customers
   end
+
+  def new(conn, params) do
+
+  end
+
+  def show(conn, %{"id" => customer_id}) do
+    customer = Customer.load(customer_id)
+    render conn, "show.html", customer: customer
+  end
+
+  def edit(conn, params) do
+
+  end
+
+  def create(conn, params) do
+
+  end
+
+  def update(conn, params) do
+
+  end
+
+  def delete(conn, %{"id" => customer_id}) do
+      Repo.get!(Customer, customer_id)
+      |> Repo.delete!
+
+      conn
+      |> put_flash(:info, "Customer Deleted")
+      |> redirect(to: customer_path(conn, :index))
+  end
 end
